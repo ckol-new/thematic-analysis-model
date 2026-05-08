aaf5e977811d9b33d291612c67bfcc1a7bd56048
ckol <christopher.kollar@ucalgary.ca>
2026-05-08 14:32:00 -0600

    generate_seeds() unit test implemented and working

---
+++ tests/test_scraping_pipeline.py
@@ -0,0 +1,27 @@
+from thematic_analysis_model.model.scraping_pipeline import *
+import pytest
+
+# test seed generator
+def test_generate_seeds():
+    base = 'www.testWebsite/p'
+    end_seq = '/testing.com'
+
+    expected = [
+        'www.testWebsite/p1/testing.com',
+        'www.testWebsite/p2/testing.com',
+        'www.testWebsite/p3/testing.com',
+        'www.testWebsite/p4/testing.com',
+        'www.testWebsite/p5/testing.com',
+    ]
+    expected2 = [
+        'www.testWebsite/p10/testing.com',
+        'www.testWebsite/p11/testing.com',
+        'www.testWebsite/p12/testing.com',
+        'www.testWebsite/p13/testing.com',
+        'www.testWebsite/p14/testing.com',
+    ]
+    output = generate_seeds(base, 1, 5, end_seq)
+    output2 = generate_seeds(base, 10, 14, end_seq)
+
+    assert expected == output
+    assert expected2 == output2
