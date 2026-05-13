ad9b27e9923b6ff75a4b408a33c7b7071b4cd4af
ckol-new <christopher.j.kollar@gmail.com>
2026-05-13 15:21:02 -0600

    implemented embedded data classes

--- src/thematic_analysis_model/model/dclasses.py
+++ src/thematic_analysis_model/model/dclasses.py
@@ -1,4 +1,5 @@
 from pydantic.dataclasses import dataclass
+import numpy as np
 from pydantic import Field
 from abc import ABC
 from typing import Optional
@@ -35,3 +36,19 @@ class Comment(Content):
 class Post(Content):
     title: str
     comments: Optional[list[Comment]] = Field(default=list)
+
+
+# now sentences are embedded as np.ndarrays, therefore content is now a list of np.ndarrays
+@dataclass
+class EmbeddedContent(ABC):
+    metadata: Metadata
+    embedded_content: list[np.ndarray]
+
+@dataclass
+class EmbeddedComment(EmbeddedContent):
+    embedded_comments: Optional[list['EmbeddedComment']] = Field(default=list) # forward reference, default empty list
+
+@dataclass
+class EmbeddedPost(EmbeddedContent):
+    embedded_title: list[np.ndarray]
+    embedded_comments: Optional[list[EmbeddedComment]] = Field(default=list) # forward reference, default empty list
