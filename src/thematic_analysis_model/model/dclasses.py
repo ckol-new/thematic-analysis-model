9e40ab04112cef47398c00fd9e7228cb44850e2c
ckol-new <christopher.j.kollar@gmail.com>
2026-05-13 17:26:33 -0600

    serialization and deserialization of embedded posts working, unit tests written, equality methods for embedded dclasses is added

--- src/thematic_analysis_model/model/dclasses.py
+++ src/thematic_analysis_model/model/dclasses.py
@@ -5,7 +5,6 @@ from pydantic import Field, BeforeValidator, PlainSerializer, RootModel, ConfigD
 from abc import ABC
 from typing import Optional
 
-
 # author dataclass
 @dataclass
 class Author:
@@ -32,25 +31,11 @@ class Content(ABC):
 class Comment(Content):
     comments: Optional[list['Comment']] = Field(default=list) # forward reference to Comment class, or default to empty list
 
-# post dataclass
+# post dataclas
 @dataclass
 class Post(Content):
     title: str
     comments: Optional[list[Comment]] = Field(default=list)
-
-# smart compare for embedded data classes
-def smart_compare(val1, val2):
-    if type(val1) != type(val2):
-        return False
-
-    if isinstance(val1, np.ndarray):
-        return np.array_equal(val1, val2)
-    elif isinstance(val1, list):
-        return len(val1) == len(val2) and all(smart_compare(a, b) for a, b in zip(val1, val2))
-    elif isinstance(val1, dict):
-        return val1.keys() == val2.keys() and all(smart_compare(val1[k], val2[k]) for k in val1)
-    else:
-        return val1 == val2
 
 # annotated types for embedded dataclass serialization/deserialization
 #NOTE it is important to do this to not have to use python lists (very slow)
@@ -59,7 +44,7 @@ def smart_compare(val1, val2):
 #   plain serializer forces np.ndarray to be a python list
 CustomNpArray = Annotated[
     np.ndarray,
-    BeforeValidator(lambda v: np.ndarray(v) if not isinstance(v, np.ndarray) else v),
+    BeforeValidator(lambda v: np.array(v) if not isinstance(v, np.ndarray) else v),
     PlainSerializer(lambda v: v.tolist(), return_type=list)
 ]
 
@@ -69,12 +54,12 @@ CustomNpArray = Annotated[
 @dataclass(config=ConfigDict(arbitrary_types_allowed=True))
 class EmbeddedContent(ABC): # inherits form root model for better serialization of nested complex data types
     metadata: Metadata
-    embedded_content: list[CustomNpArray]
+    embedded_content: Optional[list[CustomNpArray]] = Field(default=list)
 
     def __eq__(self, other):
         if not isinstance(other, EmbeddedContent): return False
         if self.metadata != other.metadata: return False
-        if len(self.embedded_conten) != len(other.embedded_content): return False
+        if len(self.embedded_content) != len(other.embedded_content): return False
         for t1, t2 in zip(self.embedded_content, other.embedded_content):
             if isinstance(t1, np.ndarray):
                 if not np.array_equal(t1, t2): return False
@@ -82,14 +67,41 @@ class EmbeddedContent(ABC): # inherits form root model for better serialization 
 
         return True
 
-
-
 @dataclass(config=ConfigDict(arbitrary_types_allowed=True))
 class EmbeddedComment(EmbeddedContent):
     embedded_comments: Optional[list['EmbeddedComment']] = Field(default=list) # forward reference, default empty list
 
+    def __eq__(self, other):
+        if not isinstance(other, EmbeddedComment): return False
+        if self.metadata != other.metadata: return False
+        if len(self.embedded_content) != len(other.embedded_content): return False
+        for t1, t2 in zip(self.embedded_content, other.embedded_content):
+            if isinstance(t1, np.ndarray):
+                if not np.array_equal(t1, t2): return False
+            elif t1 != t2: return False
+        if self.embedded_comments != other.embedded_comments: return False
+        return True
+        
+
 @dataclass(config=ConfigDict(arbitrary_types_allowed=True))
 class EmbeddedPost(EmbeddedContent):
-    embedded_title: list[CustomNpArray]
+    embedded_title: Optional[list[CustomNpArray]] = Field(default=list)
     embedded_comments: Optional[list[EmbeddedComment]] = Field(default=list) # forward reference, default empty list
 
+    def __eq__(self, other):
+        if not isinstance(other, EmbeddedPost): return False
+        if self.metadata != other.metadata: return False
+        # check embedded content equality
+        if len(self.embedded_content) != len(other.embedded_content): return False
+        for t1, t2 in zip(self.embedded_content, other.embedded_content):
+            if isinstance(t1, np.ndarray):
+                if not np.array_equal(t1, t2): return False
+            elif t1 != t2: return False
+        # check embedded title equality
+        if len(self.embedded_title) != len(other.embedded_title): return False
+        for t1, t2 in zip(self.embedded_title, other.embedded_title):
+            if isinstance(t1, np.ndarray):
+                if not np.array_equal(t1, t2): return False
+            elif t1 != t2: return False
+        if self.embedded_comments != other.embedded_comments: return False
+        return True
