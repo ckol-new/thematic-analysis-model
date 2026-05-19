7c47ace495bbb7d265295ed41c534b7c6faa6760
ckol-new <christopher.j.kollar@gmail.com>
2026-05-19 10:36:36 -0600

    ported over Author, Metadata, Content, Comment, and Post dataclasses in dclasses.py

---
+++ src/thematic_analysis_model/model/dclasses.py
@@ -0,0 +1,40 @@
+from pydantic.dataclasses import dataclass
+from typing import Annotated
+import numpy as np
+from pydantic import Field, BeforeValidator, PlainSerializer, RootModel, ConfigDict
+from abc import ABC
+from typing import Optional
+
+
+# author dataclass
+@dataclass(config=ConfigDict(frozen=True))
+class Author:
+    username: str
+    userid: str
+
+# metadata dataclass
+@dataclass(config=ConfigDict(frozen=True))
+class Metadata:
+    uuid: str
+    author: Author
+    url: str
+    date: str
+    origin: str
+    date_accessed: str
+
+# content abstract dataclass
+@dataclass
+class Content(ABC):
+    metadata: Metadata
+    content: str
+
+# comment dataclass
+@dataclass
+class Comment(Content):
+    comments: Optional[list['Comment']] = Field(default_factory=list) # forward reference to Comment class, or default to empty list
+
+# post dataclas
+@dataclass
+class Post(Content):
+    title: str
+    comments: Optional[list[Comment]] = Field(default=list)
