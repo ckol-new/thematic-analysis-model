d438322ebde9d395f12268c6f4ed7da93f5f7486
ckol-new <christopher.j.kollar@gmail.com>
2026-05-19 10:38:15 -0600

    ported test_dclasses.py for Author, Metadata, Comment, and Post

---
+++ tests/test_dclasses.py
@@ -0,0 +1,110 @@
+from thematic_analysis_model.model.dclasses import *
+from pydantic.dataclasses import dataclass
+from pydantic import RootModel
+from dataclasses import asdict
+from pydantic import TypeAdapter
+import pytest
+import uuid
+import json
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+test_var = {
+    'uuid': '123',
+    'url': 'test.com',
+    'date': 'today',
+    'origin': 'forum.com',
+    'date_accessed': 'right now',
+    'username': 'user',
+    'userid': 'userid',
+    'content': 'text',
+    'title': 'title',
+}
+
+def test_author():
+    username = 'user'
+    userid = 'userid'
+    author = Author(username, userid)
+
+    adapter = TypeAdapter(Author)
+
+    # expected serialized output
+    expected = {"username":"user", "userid":"userid"}
+    data = json.loads(adapter.dump_json(author).decode())
+    assert expected == data
+
+    # test if loading from json works
+    input_str = '{"username":"user", "userid":"userid"}'
+    author2 = adapter.validate_json(input_str)
+    assert author == author2
+    
+def test_metadata():
+    uuid = '123'
+    url = 'test.com'
+    date = 'today'
+    origin = 'forum.com'
+    date_accessed = 'right now'
+
+    username = 'user'
+    userid = 'userid'
+    author = Author(username, userid)
+
+    metadata = Metadata(uuid, author, url, date, origin, date_accessed)
+    adapter = TypeAdapter(Metadata)
+    
+    # test serialization
+    expected = {'uuid': '123', 'author': {'username': 'user', 'userid': 'userid'}, 'url': 'test.com', 'date': 'today', 'origin': 'forum.com', 'date_accessed': 'right now'}
+    data = json.loads(adapter.dump_json(metadata).decode())
+    assert expected == data
+
+    # test deserialization
+    #NOTE it is important for the string to have "" for any json or it will fail; python excepts both but json does not
+    input_str = {'uuid':'123', 'author':{'username':'user', 'userid':'userid'}, 'url':'test.com', 'date':'today', 'origin':'forum.com', 'date_accessed':'right now'}
+    metadata2 = adapter.validate_python(input_str)
+    assert metadata == metadata2
+
+
+# if post is working, comment is also working
+# also tests nesting
+def test_post():
+    a = Author(test_var['username'], test_var['userid'])
+    m = Metadata(uuid=test_var['uuid'], author=a, url=test_var['url'], date=test_var['date'], origin=test_var['origin'], date_accessed=test_var['date_accessed'])
+    c = Comment(m, test_var['content'], None)
+    p = Post(m, test_var['content'], test_var['title'], [c])
+
+    adapter = TypeAdapter(Post)
+
+    # test serialization
+    expected = {'metadata': {'uuid': '123', 'author': {'username': 'user', 'userid': 'userid'}, 'url': 'test.com', 'date': 'today', 'origin': 'forum.com', 'date_accessed': 'right now'}, 'content': 'text', 'title': 'title', 'comments': [{'metadata': {'uuid': '123', 'author': {'username': 'user', 'userid': 'userid'}, 'url': 'test.com', 'date': 'today', 'origin': 'forum.com', 'date_accessed': 'right now'}, 'content': 'text', 'comments': None}]}
+    data = json.loads(adapter.dump_json(p))
+    print(data)
+    assert expected == data
+
+    # test deserialization
+    input_json = adapter.dump_json(p).decode()
+    p2 = adapter.validate_json(input_json)
+    assert p == p2
+
+#tests if posts work if there are no comments
+def test_post_nocomments():
+    a = Author(test_var['username'], test_var['userid'])
+    m = Metadata(uuid=test_var['uuid'], author=a, url=test_var['url'], date=test_var['date'], origin=test_var['origin'], date_accessed=test_var['date_accessed'])
+    p = Post(m, test_var['content'], test_var['title'], None)
+
+    adapter = TypeAdapter(Post)
+
+    # test serialization
+    expected = {'metadata': {'uuid': '123', 'author': {'username': 'user', 'userid': 'userid'}, 'url': 'test.com', 'date': 'today', 'origin': 'forum.com', 'date_accessed': 'right now'}, 'content': 'text', 'title': 'title', 'comments': None}
+    data = json.loads(adapter.dump_json(p))
+    assert expected == data
+
+    # test deserilaization
+    input_json = adapter.dump_json(p).decode()
+    p2 = adapter.validate_json(input_json)
+    assert p == p2
+    
+
+
+
+
+
