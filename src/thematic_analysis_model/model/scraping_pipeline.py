c518fdf6d57759e278a40736848817c910a387f0
ckol-new <christopher.j.kollar@gmail.com>
2026-05-19 11:14:42 -0600

    added generate_seeds() util to scraping_pipeline.py, and tests for it

---
+++ src/thematic_analysis_model/model/scraping_pipeline.py
@@ -0,0 +1,15 @@
+from thematic_analysis_model.model.dclasses import *
+from thematic_analysis_model.model.util import *
+
+# UTILITY functions
+# seed generator utility helps speed up process of generating seeds, which act as start nodes for the crawler to branch out from.
+def generate_seeds(base: str, start: int, stop: int, end_seq: str) -> list[str]:
+    seeds: list = []
+
+    for i in range(start, stop + 1):
+        seed: str = base + f'{i}' + end_seq
+        seeds.append(seed)   
+
+    return seeds
+
+
