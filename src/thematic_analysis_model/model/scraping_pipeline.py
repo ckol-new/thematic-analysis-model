8369315bd917373f6e8685cb7f584a6bcb8bd60b
ckol-new <christopher.j.kollar@gmail.com>
2026-05-19 12:09:58 -0600

    added run_crawler, run_scraper, and scrape, and run_pipeline methods to Scraping Pipeline

--- src/thematic_analysis_model/model/scraping_pipeline.py
+++ src/thematic_analysis_model/model/scraping_pipeline.py
@@ -3,6 +3,9 @@ from thematic_analysis_model.model.util import *
 from abc import ABC, abstractmethod
 import codecs
 import requests
+import datetime
+from bs4 import BeautifulSoup
+import uuid
 
 # UTILITY functions
 # seed generator utility helps speed up process of generating seeds, which act as start nodes for the crawler to branch out from.
@@ -15,11 +18,36 @@ def generate_seeds(base: str, start: int, stop: int, end_seq: str) -> list[str]:
 
     return seeds
 
+# can instantiate the scraping pipeline to be empty, however it will not run. It is on the user to ensure all data is prewsent to allow scraping pipeline to run.
 class ScrapingPipeline(ABC):
-    def __init__(self):
-        ...
+    def __init__(self, seeds: list[str] | None = None, crawl_save_location: Path | None = None, scrape_save_location: Path | None = None, forum_origin: str | None = None):
+        self.seeds = seeds
+        self.crawl_save_location = crawl_save_location
+        self.scrape_save_location = scrape_save_location
+        self.forum_origin = forum_origin
+        self.crawl_output = None
+        self.scrape_output = None
+        
 
     # main methods
+
+    # run pipeline method
+    def run_pipeline(self):
+        # run crawler
+        print('RUNNING CRAWLER')
+        self.crawl_output = self.run_crawler()
+
+        # optionally save scrape output
+        if self.crawl_save_location: 
+            self.save_crawl_output()
+
+        print("RUNNING SCRAPER")
+        # run scraper
+        self.scrape_output = self.run_scraper()
+
+        # optionally save scrape output
+        if self.scrape_save_location: 
+            self.save_scrape_output()
 
     # run_crawler method acts as 'queue' of all crawl operations to be performed on each 'seed' or start node
     # generate crawl output which is the list of all pages to be scraped from
@@ -32,6 +60,67 @@ class ScrapingPipeline(ABC):
             crawl_output = crawl_output + output
 
         return crawl_output
+    
+    # run scraper method acts as 'queue', running many scraping operations for every link in the crawl output
+    def run_scraper(self) -> list[Post] | None:
+        scrape_output = []
+        count = 0
+        total = len(self.crawl_output)
+        for crawl_seed in self.crawl_output:
+            count += 1
+            if count % 10 == 0:
+                print(f'% {100*(count / total)} finished')
+            try:
+                scraped_post: Post = self.scrape(crawl_seed)
+            except Exception as e:
+                print(f'ran into issue at {crawl_seed} exception: {e}')
+            # validation step
+            if not self.validate_post(scraped_post): continue
+            scrape_output.append(scraped_post)
+        
+        if not scrape_output: return None
+        return scrape_output
+
+      # scrape is a single scraping operation for a single link. It corresponds to the scraping of one single forum post.
+    # is unique to each forum.
+    def scrape(self, url: str) -> Post | None:
+        # get page
+        #TODO implement customization of header
+        page_html = ScrapingPipeline.request_page(url)
+        # get soup object
+        soup = BeautifulSoup(page_html, 'html.parser')
+        
+        # get title
+        title = self.scrape_title(soup)
+
+        # get content
+        content = self.scrape_content(soup)
+
+        # get metadata
+        # get unique id
+        my_uuid = str(uuid.uuid4())
+
+        #   get date
+        date = self.scrape_date(soup)
+        # get date accessed
+        now = datetime.now()
+        date_accessed = now.strftime("%Y-%m-%d %H:%M:%S")
+
+        #   get author 
+        #       get user name
+        username = self.scrape_username(soup)       
+        #       get user id
+        userid = self.scrape_userid(soup)
+
+        #   get comments: recursively call
+        comments = self.scrape_comments(soup, url, self.forum_origin)
+
+        # create objects, must package comments inside post object
+        author = Author(username, userid)
+        metadata = Metadata(my_uuid, author, url, date, self.forum_origin, str(date_accessed))
+        post = Post(metadata, content, title, comments)
+
+        return post
 
 
     # file i/o for seeds
