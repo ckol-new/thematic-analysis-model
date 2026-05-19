de0739dfaaa7448f1115922d7db5aceb09c43d12
ckol-new <christopher.j.kollar@gmail.com>
2026-05-19 12:03:52 -0600

    added all abstract methods  + imported request pages + imported run_crawler_method ot ScrapingPipeline(ABC)

--- src/thematic_analysis_model/model/scraping_pipeline.py
+++ src/thematic_analysis_model/model/scraping_pipeline.py
@@ -2,6 +2,7 @@ from thematic_analysis_model.model.dclasses import *
 from thematic_analysis_model.model.util import *
 from abc import ABC, abstractmethod
 import codecs
+import requests
 
 # UTILITY functions
 # seed generator utility helps speed up process of generating seeds, which act as start nodes for the crawler to branch out from.
@@ -17,6 +18,21 @@ def generate_seeds(base: str, start: int, stop: int, end_seq: str) -> list[str]:
 class ScrapingPipeline(ABC):
     def __init__(self):
         ...
+
+    # main methods
+
+    # run_crawler method acts as 'queue' of all crawl operations to be performed on each 'seed' or start node
+    # generate crawl output which is the list of all pages to be scraped from
+    def run_crawler(self) -> list[str]:
+        # iterate through seeds, to request each page html
+        crawl_output = []
+
+        for seed in self.seeds:
+            output = self.crawl(seed)           
+            crawl_output = crawl_output + output
+
+        return crawl_output
+
 
     # file i/o for seeds
     @classmethod
@@ -41,6 +57,30 @@ class ScrapingPipeline(ABC):
     @classmethod
     def load_scrape_output(cls, fpath: Path) -> list[Post]:
         return load_dclasses(file_path=fpath, cls=Post)
+    
+
+    # request page
+    # returns html of page as string
+    # optionally pass in header, otherwise it uses default
+    @classmethod
+    def request_page(cls, url: str, header: dict = None) -> str:
+        if not header:
+            header = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+            }
+        try:
+            response = requests.get(url, headers=header)
+            if response.status_code == 200:
+                ...
+            else: print(f'REQUEST FAILED for {url}: ', response.status_code)
+
+            html_text = response.text
+            response.close() # close connection to server I think
+
+            return html_text
+
+        except requests.exceptions.RequestException as e:
+            raise Exception(e)
     
     # method for processing text, and removing escape characterws
      # pre-process text
@@ -89,3 +129,32 @@ class ScrapingPipeline(ABC):
         if not comment.metadata.date_accessed: return False
 
         return True
+
+    
+    # abstract methods for getting each componenet of the post: implemented by each specific scraping pipeline
+
+    # crawl method is specific to each forum
+    @abstractmethod
+    def crawl(self, seed:str) -> list[str] | None:
+        ...
+    @abstractmethod
+    def scrape_title(self, soup):
+        ...
+    # content will be stored as a string with newline characters separating sentences
+    @abstractmethod
+    def scrape_content(self, soup):
+        ...
+    @abstractmethod
+    def scrape_date(self, soup):
+        ...
+    @abstractmethod
+    def scrape_username(self, soup):
+        ...
+    @abstractmethod
+    def scrape_userid(self, soup):
+        ...
+    # scrape comments plural
+    #   must be able to return None if necessary
+    @abstractmethod
+    def scrape_comments(self, soup: BeautifulSoup, url: str, forum_origin: str) -> list[Comment] | None:
+        ...
