ba949e4e5eda3619b7aaa4a069ec4f1100a65da5
ckol-new <christopher.j.kollar@gmail.com>
2026-05-24 19:02:53 -0600

    made dataclass fields optional (some of them)

--- src/thematic_analysis_model/model/dclasses.py
+++ src/thematic_analysis_model/model/dclasses.py
@@ -1,22 +1,24 @@
 from pydantic import BaseModel
+from typing import Optional
 from pydantic.dataclasses import dataclass
 from lancedb.pydantic import LanceModel
 
 class Author(BaseModel):
-    username: str
-    userid: str
+    username: Optional[str] = None
+    userid: Optional[str] = None
 
 class Metadata(BaseModel):
     url: str
     uuid: str
     date: str
     date_accessed: str
-    author: Author
+    author: Optional[Author] = None
     origin: str
 
 class Content(LanceModel):
     metadata: Metadata
     content: str
+    title: Optional[str] = None
     content_type: str
 
 
