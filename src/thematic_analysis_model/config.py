21690faca60a95101f3691ea3074068919de8acb
ckol-new <christopher.j.kollar@gmail.com>
2026-05-24 19:13:05 -0600

    added util, config files, and main. Created LanceDB

---
+++ src/thematic_analysis_model/config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+base = Path.cwd()
+
+LDB_PATH = base / 'lance_db'
+SCRAPE_DATA_TABLE_NAME = 'scrape_data'
--- src/thematic_analysis_model/main.py
+++ src/thematic_analysis_model/main.py
@@ -1,7 +1,10 @@
-
+import lancedb as ldb
+from thematic_analysis_model.config import LDB_PATH, SCRAPE_DATA_TABLE_NAME
+from thematic_analysis_model.model.dclasses import Content
 
 def main():
-    ...
+    db = ldb.connect(LDB_PATH)
+    table = db.create_table(name=SCRAPE_DATA_TABLE_NAME, schema=Content)
 
 if __name__ == '__main__':
     main()
