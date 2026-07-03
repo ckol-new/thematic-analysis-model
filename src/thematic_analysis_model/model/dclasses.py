4391b7c847fc9f2a8fd1ee5f5806c8557313d1ac
ckol-new <christopher.j.kollar@gmail.com>
2026-07-03 12:08:09 -0600

    Added new dataclass to track documents during validation

--- src/thematic_analysis_model/model/dclasses.py
+++ src/thematic_analysis_model/model/dclasses.py
@@ -1,9 +1,11 @@
 from lancedb.pydantic import LanceModel, Vector
+from pydantic import BaseModel
 import pyarrow as pa
 from typing import Optional
 from ..config import SENTENCE_EMBEDDING_DIMENSIONS
 
 
+# schema and dataclass as one class. Acts as scheme for content LanceDB, where data is stored in its more native form.
 class SchemaContent(LanceModel):
     url: str
     url_hash: str 
@@ -17,6 +19,7 @@ class SchemaContent(LanceModel):
     content_type: str
     is_split: bool
 
+# schema and dataclass as one. Acts as schema for Sentence LanceDB< where data is stored at the sentence level. This acts as corpus for training.
 class SchemaSentence(LanceModel):
     url: str
     url_hash: str 
@@ -28,3 +31,10 @@ class SchemaSentence(LanceModel):
     vector: Vector(dim=SENTENCE_EMBEDDING_DIMENSIONS) | None
     is_embedded: bool = False
     is_modelled: bool  = False
+
+# dataclass acting as view for document for validaiton purposes.
+class DocumentTracker(BaseModel):
+    doc_id: int
+    doc_uuid: str # hexdigit uuid
+    topic: int | None
+    probs: list | None
