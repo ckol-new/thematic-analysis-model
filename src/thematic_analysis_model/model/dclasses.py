9c99773cf096caef2236172e5992d21f95294821
ckol-new <christopher.j.kollar@gmail.com>
2026-07-08 12:34:20 -0600

    implemented Content dclass

--- src/thematic_analysis_model/model/dclasses.py
+++ src/thematic_analysis_model/model/dclasses.py
@@ -1,9 +1,23 @@
 from lancedb.pydantic import LanceModel
 from pydantic import BaseModel, TypeAdapter
+from enum import Enum
+
+class ContentType(Enum):
+    POST = 1
+    COMMENT = 1
 
 # dataclass for content (either post or comment), as close to its native context as possible
 class Content:
-    ...
+    text: str
+    url: str
+    date: str
+    title: str | None
+    author_username: str
+    forum_origin: str
+    hash_: str
+    uuid: str
+    type_: ContentType
+    is_processed: bool = False
 
 # dataclass for individual line of post/comment, with all necessary metadata
 class Line:
