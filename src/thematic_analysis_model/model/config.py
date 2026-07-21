c5d15fd12f3dc41a1a37940bb764bb6d4e28670d
ckol-new <christopher.j.kollar@gmail.com>
2026-07-21 15:46:57 -0600

    Added config vars

---
+++ src/thematic_analysis_model/model/config.py
@@ -0,0 +1,11 @@
+# config file for global vars
+from pathlib import Path
+
+# Paths
+DATABASE_PATH = Path.cwd() / 'db'
+
+# Global Vars
+CONTENT_TBL_NAME = "content"
+SENTENCE_TBL_NAME = "sentence"
+MODEL_OUTPUT_TBL_NAME = "model_output"
+
