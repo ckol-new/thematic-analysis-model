d49d1533eff14754dc3d619b4c0dde2c9a859fae
ckol-new <christopher.j.kollar@gmail.com>
2026-07-23 12:17:27 -0600

    outlining Embedder class

--- src/thematic_analysis_model/model/embedding.py
+++ src/thematic_analysis_model/model/embedding.py
@@ -1,1 +1,17 @@
 # embedding data
+#   Class for embedding sentences
+class Embedder:
+    def __init__(self):
+        pass
+
+    # main entry
+    def run_embedder(self):
+        # get batches of unembedded sentences
+        # embed batches
+        # save batches + update bools
+        ...
+
+    # embeds batch of sentences
+    def embed_batch(self):
+        # embed
+        ...
